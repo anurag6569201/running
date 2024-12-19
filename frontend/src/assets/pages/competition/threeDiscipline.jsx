6f185affc520c115e5648d1df00ed1504fe1ffd5
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-12-20 00:31:01 +0530

    updated all pages

--- frontend/src/assets/pages/competition/threeDiscipline.jsx
+++ frontend/src/assets/pages/competition/threeDiscipline.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import DesignBar from '../home/design_bar';
+import Footer from '../home/footer';
+
 
 function ThreeDiscipline() {
   return (
-    <div>
-      <h1>ThreeDiscipline</h1>
-      <p>This is the ThreeDiscipline page.</p>
-    </div>
+    <>
+      <div className="container">
+        <div className="row justify-content-center mb-5">
+          <div className="col-md-8 text-center mb-4">
+            <span>The three disciplines</span>
+            <h1>Running <span>*</span> Jumping <br />Throwing</h1>
+            <p>Every child knows these three forms of movement just from playing with their friends. They are required in almost every sport and are practised and perfected in athletics.</p>
+          </div>
+          <div className="col-md-8 d-flex justify-content-center">
+            <img className='desc_img' src="images/compt/threeDesc.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <DesignBar />
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-12">
+            <h2 className='text-center'>The Athletics Kids Cup is a triathlon consisting of the following disciplines</h2>
+          </div>
+          <div className="col-md-12 d-flex small_desc_btn mt-3">
+            <span className="shadow_v1">Sprint60m - 1 attempt</span>
+            <span className="shadow_v1">Long Jump with Zone Jump - 3 attempts</span>
+            <span className="shadow_v1">Ball Throw 200 g - 3 attempts</span>
+          </div>
+        </div>
+      </div>
+      <DesignBar />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5">
+            <h1>Sprint 60 m</h1>
+            <p>Whether you use the low start, as the pros do, or the "easier" high start. The objective is clear: sprint to the finish line as fast as possible! Attention: Don't start too early! Anyone can make a false start. But if anyone starts a second time before the starting signal, unfortunately they will be disqualified.</p>
+            <span></span>
+          </div>
+          <div className="col-md-5">
+            <img src="images/compt/three1.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/assets/pages/competition/whoParticipate.jsx
+++ frontend/src/assets/pages/competition/whoParticipate.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Section3 from '../home/section3';
+import Footer from '../home/footer';
+
 
 function WhoParticipate() {
   return (
     <div>
-      <h1>WhoParticipate</h1>
-      <p>This is the WhoParticipate page.</p>
+      <div className="container particcontainer mb-5">
+        <div className="row partic_row_wrapper">
+          <div className="col-md-6">
+            <img src="images/compt/parT1.png" alt="" />
+          </div>
+          <div className="col-md-6">
+            <h1>Who can participate?</h1>
+            <p>Do you want to take part in a sporting competition with your age group and have a lot of fun with your friends? Were you born in 2009 or younger? Then the Athletics Kids Cup is just the thing for you. In the Athletics Kids Cup you compete in three disciplines</p>
+            <ul>
+                <li><div className="span_circle shadow_v1"></div> 60m Sprint</li>
+                <li><div className="span_circle shadow_v1"></div>Long Jump</li>
+                <li><div className="span_circle shadow_v1"></div>Ball Throw</li>
+              </ul>
+            <p>All three disciplines together give you your score. If you would like to take part in one of our competitions, all you have to do is convince the responsible persons at your school to organise an Athletics Kids Cup.</p>
+          </div>
+        </div>
+        <Section3/>
+      </div>
+        <Footer/>
     </div>
   );
 }
