efa2f933d6a3cc8a9c836f6586abca6861339f52
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-12-24 21:56:01 +0530

    updated setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from contact import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,7 @@ urlpatterns = [
     path('api/', include('contact.urls')), 
     path('api/', include('register.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
