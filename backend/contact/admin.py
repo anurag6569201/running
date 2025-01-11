cd7651a83d9d3f126e3686d39dd71c2c628d6288
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-01-11 10:16:09 +0530

    feat(updated backend)

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import ContactForms
+from import_export.admin import ImportExportModelAdmin
 
-class ContactAdmin(admin.ModelAdmin):
+class ContactAdmin(ImportExportModelAdmin):
     list_display = ('full_name', 'phone', 'email', 'subject')
     search_fields = ('full_name', 'phone', 'email', 'subject')
     list_filter = ('email',)
--- backend/register/admin.py
+++ backend/register/admin.py
@@ -1,8 +1,10 @@
 # admin.py
 from django.contrib import admin
 from .models import SchoolRegistration
+from import_export.admin import ImportExportModelAdmin
 
-class SchoolAdmin(admin.ModelAdmin):
+
+class SchoolAdmin(ImportExportModelAdmin):
     list_display = ('school_name', 'city', 'state', 'pincode', 'first_name', 'surname', 'email')
     search_fields = ('school_name', 'city', 'state', 'first_name', 'surname', 'email')
     list_filter = ('state',)
